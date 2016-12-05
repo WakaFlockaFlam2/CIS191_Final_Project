fea8ee1dd022f382265a445e4902c8d71d51f896
WakaFlockaFlam2 <jacob.beckerman@gmail.com>
2016-12-04 20:17:15 -0500

    implemented more radio boxes for version

--- main.py
+++ main.py
@@ -18,19 +18,41 @@ class simpleapp_wx(wx.Frame):
         print "Second radioBtn = ", self.radio2.GetValue()
     def quit(self, event):
         sys.exit()
+    def update(self, event):
+        self.progText.SetLabel("System is up to date for ROS...")
+        os.system("gnome-terminal -e 'sudo apt-get update'")
+        self.g1.SetValue(30)
 
+    def onRadioBox(self,e): 
+        print self.box.GetStringSelection(),' is clicked from Radio Box'
+
+    def onClickWorkspace(self, e): 
+      cb = e.GetEventObject() 
+      print cb.GetLabel(),' is clicked',cb.GetValue()
     def initialize(self):
         jpg = wx.Image('ros_install_header.jpg', wx.BITMAP_TYPE_JPEG).ConvertToBitmap()
         wx.StaticBitmap(self, -1, jpg, (0, 0))
+        
+        self.box = wx.RadioBox(self, -1, "ROS Distro        ", pos=(200,200), choices=["kinetic", "jade", "indigo", "hydro", "groovy", "fuerte"], style=wx.VERTICAL)
+        self.box.Bind(wx.EVT_RADIOBOX,self.onRadioBox)
 
-        self.radio = wx.RadioButton(self, label="Test", style = wx.RB_GROUP, pos=(200,200))
-        self.radio2 = wx.RadioButton(self, label="Test2", pos=(200,220))
+        self.libs = wx.RadioBox(self, -1, "ROS Version        ", pos=(400,200), choices=["full", "desktop", "base"], style=wx.VERTICAL)
+        self.libs.Bind(wx.EVT_RADIOBOX,self.onRadioBox)
+
+        self.workspace = wx.CheckBox(self, -1, label="Initialize workspace", pos=(600, 200))
+        self.workspace.SetValue(True)
+        self.workspace.Bind(wx.EVT_CHECKBOX, self.onClickWorkspace)
+
         installbtn = wx.Button(self, label="Install", pos=(900, 650))
-        installbtn.Bind(wx.EVT_BUTTON, self.onBtn)
+        installbtn.Bind(wx.EVT_BUTTON, self.update)
         quitbutton = wx.Button(self, label="Quit", pos=(800, 650))
         quitbutton.Bind(wx.EVT_BUTTON, self.quit)
-        
 
+        self.infoText = wx.StaticText(self, -1, "ROS is a middleware for asynchronous, one-to-many communication between nodes.", (400, 400))
+        self.progText = wx.StaticText(self, -1, "Install Progress:", (50, 550))
+
+        self.g1 = wx.Gauge(self, -1, 100, (50, 580), (900, 45))        
+        self.g1.SetValue(0)
         self.Show(True)
 
 if __name__ == "__main__":
