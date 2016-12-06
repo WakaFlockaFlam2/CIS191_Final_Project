ab77c31e9d2b55f6306bae0056e2bb05b440c6c8
WakaFlockaFlam2 <jacob.beckerman@gmail.com>
2016-12-05 23:23:47 -0500

    final commit before project due - added readme updates

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 #! /usr/bin/python
 
-# -*- coding: iso-8859-1 -*-
+#these are system libs no install required
+import os
+import sys
+import lsb_release
 try:
     import wx
-    import os
-    import sys
-    import lsb_release
 except ImportError:
-    raise ImportError,"The wxPython module is required to run this program"
+    raise ImportError,"Need GUI Library. Run $sudo apt-get install python-wxgtk2.8on"
 class simpleapp_wx(wx.Frame):
     def __init__(self,parent,id,title):
         wx.Frame.__init__(self,parent,id,title)
         self.parent = parent
         self.initialize()
+    #quit button calls this function
     def quit(self, event):
         sys.exit()
-    def update(self, event):
+    #update() is called when install is clicked to install ROS
+    def update(self, event): 
         print
         self.progText.SetLabel("Install Progress:")
         if self.box.GetStringSelection() == "hydro" or self.box.GetStringSelection() == "groovy" or self.box.GetStringSelection() == "fuerte":
@@ -31,10 +33,13 @@ class simpleapp_wx(wx.Frame):
             os.system("gnome-terminal -e 'sudo apt-get update'")
             self.g1.SetValue(30)
             os.system("sudo apt-get install ros-" + self.box.GetStringSelection() + "-" + self.libs.GetStringSelection() + " && sudo rosdep init && rosdep update && echo 'source /opt/ros/indigo/setup.bash' >> ~/.bashrc && source ~/.bashrc && sudo apt-get install python-rosinstall && mkdir -p ~/catkin_ws/src && cd ~/catkin_ws/src && catkin_init_workspace")
+            self.g1.SetValue(60)
             if self.tuts.GetLabel():
                 os.system("gnome-terminal -e 'sudo apt-get install ros-" + self.box.GetStringSelection() + "-ros-tutorials'")
-            self.g1.SetValue(60)
+            self.g1.SetValue(100)
+            self.progText.SetLabel("Complete!")
 
+    #some functions for testing radio box implementation
     def onRadioBox(self,e): 
         print self.box.GetStringSelection()
     def onLibsBox(self,e): 
@@ -48,43 +53,42 @@ class simpleapp_wx(wx.Frame):
       print cb.GetLabel(),' is clicked',cb.GetValue()
 
     def initialize(self):
+        #add all the gui componends
         jpg = wx.Image('ros_install_header.jpg', wx.BITMAP_TYPE_JPEG).ConvertToBitmap()
         wx.StaticBitmap(self, -1, jpg, (0, 0))
-
+        #open software image
         osi = wx.Image('osilogo.png', wx.BITMAP_TYPE_PNG).ConvertToBitmap()
         wx.StaticBitmap(self, -1, osi, (600, 462), size=(100,100))
-
+        #xkcd image
         xkcd = wx.Image('xkcd.png', wx.BITMAP_TYPE_PNG).ConvertToBitmap()
         wx.StaticText(self, -1, "Obligatory  xkcd", (762, 308))
         wx.StaticBitmap(self, -1, xkcd, (725, 325))
-        
+        #distro selection
         self.box = wx.RadioBox(self, -1, "ROS Distro        ", pos=(55,225), choices=["kinetic", "jade", "indigo", "hydro", "groovy", "fuerte"], style=wx.VERTICAL)
         self.box.Bind(wx.EVT_RADIOBOX,self.onRadioBox)
         wx.StaticText(self, -1, "ROS releases are called\ndistributions.The distros are\nordered from newest to oldest.\nChoose the default option if\nunsure.", (13, 415))
-
-
+        #library install selection
         self.libs = wx.RadioBox(self, -1, "ROS Version        ", pos=(280,225), choices=["desktop-full", "desktop", "ros-base"], style=wx.VERTICAL)
         self.libs.Bind(wx.EVT_RADIOBOX,self.onLibsBox)
         wx.StaticText(self, -1, "Choose 'full-desktop' unless\nyou are short on storage.", (280, 350))
-
+        #workspace initialization checkbox
         self.workspace = wx.CheckBox(self, -1, label="Initialize workspace", pos=(505, 225))
         self.workspace.SetValue(True)
         self.workspace.Bind(wx.EVT_CHECKBOX, self.onClickWorkspace)
         wx.StaticText(self, -1, "If selected a ROS workspace\n will be initialized.", (505, 275))
-
+        #install tutorial libs checkbox
         self.tuts = wx.CheckBox(self, -1, label="Install ROS Tutorials Package", pos=(730, 225))
         self.tuts.SetValue(True)
         self.tuts.Bind(wx.EVT_CHECKBOX, self.onClickTuts)
         wx.StaticText(self, -1, "", (13, 160))
-
+        #install button which calls the update() function
         installbtn = wx.Button(self, label="Install", pos=(900, 650))
         installbtn.Bind(wx.EVT_BUTTON, self.update)
         quitbutton = wx.Button(self, label="Quit", pos=(800, 650))
         quitbutton.Bind(wx.EVT_BUTTON, self.quit)
-
+        #progress bar and info text
         self.infoText = wx.StaticText(self, -1, "ROS is a middleware for asynchronous, one-to-many communication between nodes. This ROS installer is made to simplify the process of installing\nROS on Ubuntu. It may work with some other Linux distros, but this has not been tested.", (13, 160))
         self.progText = wx.StaticText(self, -1, "Install Progress:", (50, 550))
-
         self.g1 = wx.Gauge(self, -1, 100, (50, 580), (900, 45))        
         self.g1.SetValue(0)
         self.Show(True)
